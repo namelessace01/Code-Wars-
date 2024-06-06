4e6f66ff6040511e2815f5a31b56df0b4a2b6bf9
Eniiyi Bishop Ajala <adedamolaajala33@gmail.com>
2024-06-06 18:18:49 +0100

    Test 2

--- lowest_numbers_sum.py
+++ lowest_numbers_sum.py
@@ -10,3 +10,4 @@ def sum_two_smallest_numbers(numbers):
 print(sum_two_smallest_numbers([5, 8, 12, 18, 22]), 13)
 print(sum_two_smallest_numbers([7, 15, 12, 18, 22]), 19)
 print(sum_two_smallest_numbers([25, 42, 12, 18, 22]), 30)
+
